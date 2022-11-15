5f53fe57ecd368e2ebce9f64f761b43579a103f8
Simon Stone <simon.stone@dartmouth.edu>
2022-11-15 13:43:20 -0500

    adds shebang to setup script

--- setup_jhub_user.py
+++ setup_jhub_user.py
@@ -1,3 +1,5 @@
+#!/bin/python3
+
 """ This script copies all Jupyter notebooks from the submodules into the user space 
 
 This is only required on the Dartmouth JupyterHub, where the repo is mounted as read-only.
