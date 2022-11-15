2240009cd7faea6bec7e01fe0120a356822b31f7
Simon Stone <simon.stone@dartmouth.edu>
2022-11-15 11:35:53 -0500

    adds a (placeholder) python script to set up a user environment on JHub

---
+++ setup_jhub_user.py
@@ -0,0 +1,7 @@
+""" This script copies all Jupyter notebooks from the submodules into the user space 
+
+This is only required on the Dartmouth JupyterHub, where the repo is mounted as read-only.
+"""
+
+if __name__ == "__main__":
+    raise NotImplementedError('The script is not implemented yet!')
